4116ee643d4fc1fff01d7c76d9ffb3f887802251
itou aiki <itouaiki46@gmail.com>
2024-04-16 17:11:44 +0900

    fix ゲームパッドの入力の修正
    ボタンが反応しなかったのを反応するように修正

--- QuickShot/Input.h
+++ QuickShot/Input.h
@@ -20,6 +20,17 @@ private:
 
 public:
 
+	bool InputY() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_1; }
+	bool InputB() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_2; }
+	bool InputA() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_3; }
+	bool InputX() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_4; }
+	bool InputL() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_5; }
+	bool InputR() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_6; }
+	bool InputZL() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_7; }
+	bool InputZR() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_8; }
+	bool InputMINUS() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_9; }
+	bool InputPLUS() { return GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_10; }
+
 	void Update();
 	void DebugDraw();
 };
